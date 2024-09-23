85c7431c4e05c1da61553bd769295952e68bf99b
akshaj99 <akshajsrinivasan99@Gmail.com>
2024-09-23 16:39:56 -0700

    exoplanet data

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 // Create an empty scene
 let scene = new THREE.Scene();
+const axesHelper = new THREE.AxesHelper( 1000 );
+scene.add( axesHelper );
 
 // Create a basic perspective camera
-let camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+let camera = new THREE.PerspectiveCamera(95, window.innerWidth / window.innerHeight, 1, 10000);
 camera.position.x = 200;
+
+
+
 
 // Create a renderer with Antialiasing
 let renderer = new THREE.WebGLRenderer({ antialias: true });
@@ -31,7 +36,7 @@ rawFile.onreadystatechange = function () {
         if (rawFile.status === 200 || rawFile.status == 0) {
             let data = rawFile.responseText.split('\n');
 
-            for (let i = 0; i < 4672; i++) {
+            for (let i = 0; i < 4226; i++) {
                 let parts = data[i].split(' ');
                 dotGeometry.vertices.push(
                     new THREE.Vector3(parseFloat(parts[0]), parseFloat(parts[1]), parseFloat(parts[2]))
@@ -58,7 +63,7 @@ rawFile.send(null);
 
 dotGeometry.colors = colors;
 
-let size = 0.032;
+let size = 1;
 let dotMaterial = new THREE.PointsMaterial({
     size: size,
     vertexColors: THREE.VertexColors
@@ -91,7 +96,7 @@ function onWindowResize() {
     renderer.setSize(window.innerWidth, window.innerHeight);
 }
 
-let centerindex = 0; // central galaxy
+let centerindex = 0; // Earth
 let centerdist = document.getElementById('centerdist');
 let centerdistval = 200;
 let startstring = document.getElementById('startstring');
@@ -112,12 +117,12 @@ function searchfunc() {
     let srchkey = document.getElementById('srchkey').value;
     if (srchkey == '') return;
     let key = srchkey.replace(/[^A-Z0-9]/gi, '').toLowerCase();
-    if (key == 'milkyway') {
-        updateGalaxyPage(0);
+    if (key == 'earth') {
+        updateExoplanetPage(0);
         updateCenterGUI();
     } else {
         let found = false;
-        for (let i = 0; i < 4673; i++) {
+        for (let i = 0; i < 5637; i++) {
             let name = names[i];
             if (name.indexOf(',') > -1) {
                 let namearr = name.split(',');
@@ -133,13 +138,13 @@ function searchfunc() {
                             centerindex === 0 ? new THREE.Color('#f0f') : new THREE.Color('#fff');
                     centerindex = i;
 
-                    updateGalaxyPage(i);
+                    updateExoplanetPage(i);
                     updateCenterGUI();
                     break;
                 }
             } else {
                 if (name.replace(/[^A-Z0-9]/gi, '').toLowerCase() == key) {
-                    updateGalaxyPage(i);
+                    updateExoplanetPage(i);
                     found = true;
                     break;
                 }
@@ -178,8 +183,8 @@ function onDocumentMouseClick(event) {
         if (centerindex !== idx)
             dots.geometry.colors[centerindex] = centerindex === 0 ? new THREE.Color('#f0f') : new THREE.Color('#fff');
 
-        if (idx) updateGalaxyPage(idx);
-        else updateGalaxyPage(0);
+        if (idx) updateExoplanetPage(idx);
+        else updateExoplanetPage(0);
 
         dots.geometry.colorsNeedUpdate = true;
         clickedObject = idx;
@@ -212,12 +217,12 @@ function getIntersects(x, y) {
 
     return raycaster.intersectObject(dots, true);
 }
-let galaxyName = document.getElementById('galaxyName');
-let galaxyName2 = document.getElementById('galaxyName2');
-let galaxyNames = document.getElementById('galaxyNames');
-let galaxyDist = document.getElementById('galaxyDist');
+let exoplanetName = document.getElementById('exoplanetName');
+let exoplanetName2 = document.getElementById('exoplanetName2');
+let exoplanetNames = document.getElementById('exoplanetNames');
+let exoplanetDist = document.getElementById('exoplanetDist');
 
-let galaxyColor = document.getElementById('galaxyColor');
+let exoplanetColor = document.getElementById('exoplanetColor');
 let image = document.getElementById('image');
 
 let cdsLink = document.getElementById('cds');
@@ -226,24 +231,23 @@ let aladinLink = document.getElementById('aladin');
 let ned = document.getElementById('ned');
 
 let aladinDiv = document.getElementById('aladin-lite-div');
-function updateGalaxyPage(i) {
+function updateExoplanetPage(i) {
     let name = names[i];
 
     if (i === 0) {
-        galaxyName.innerText = 'Milky Way';
-        galaxyName2.innerText = 'Milky Way';
-        galaxyNames.innerText = 'Milky Way';
+        exoplanetName.innerText = 'Earth';
+        exoplanetName2.innerText = 'Earth';
+        exoplanetNames.innerText = 'Earth';
 
-        galaxyColor.style.color = '#f0f';
-        galaxyColor.innerText = 'magenta';
+        exoplanetColor.style.color = '#f0f';
+        exoplanetColor.innerText = 'magenta';
 
         if (typeof A !== 'undefined') {
             if (aladinDiv.style.display === 'none') aladinDiv.style.display = 'block';
             if (image.style.display === 'block') image.style.display = 'none';
             A.aladin('#aladin-lite-div', { target: '266.4 -29', fov: 60, showLayersControl: false, showGotoControl: false });
         } else
-            image.src =
-                'https://upload.wikimedia.org/wikipedia/commons/thumb/4/43/ESO-VLT-Laser-phot-33a-07.jpg/1280px-ESO-VLT-Laser-phot-33a-07.jpg';
+            image.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/1d/City_Lights_of_Asia_and_Middle_East_2016.png/640px-City_Lights_of_Asia_and_Middle_East_2016.jpg';
 
         if (cdsLink.style.display === 'block') cdsLink.style.display = 'none';
         if (simbad.style.display === 'block') simbad.style.display = 'none';
@@ -252,14 +256,14 @@ function updateGalaxyPage(i) {
     } else {
         dots.geometry.colors[i] = new THREE.Color('#ff0');
 
-        galaxyColor.style.color = '#ff0';
-        galaxyColor.innerText = 'yellow';
+        exoplanetColor.style.color = '#ff0';
+        exoplanetColor.innerText = 'yellow';
 
         let firstname = name.split(',')[0];
 
-        galaxyName.innerText = firstname;
-        galaxyName2.innerText = firstname;
-        galaxyNames.innerText = name;
+        exoplanetName.innerText = firstname;
+        exoplanetName2.innerText = firstname;
+        exoplanetNames.innerText = name;
 
         if (cdsLink.style.display === 'none') cdsLink.style.display = 'block';
         if (simbad.style.display === 'none') simbad.style.display = 'block';
@@ -289,8 +293,8 @@ function updateGalaxyPage(i) {
     let from = controls.target;
     let to = dots.geometry.vertices[i];
 
-    let galaxyDistance = to.length();
-    galaxyDist.innerText = `${galaxyDistance.toFixed(1)} Mpc (${(3.262 * galaxyDistance).toFixed(1)} Mly)`;
+    let exoplanetDistance = to.length();
+    exoplanetDist.innerText = `${exoplanetDistance.toFixed(1)} Mpc (${(3.262 * exoplanetDistance).toFixed(1)} Mly)`;
 
     TWEEN.removeAll();
     let tween = new TWEEN.Tween(from)
